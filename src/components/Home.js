47d8397bc740da169fd46618c0b1a99d26906d5f
Aron Roberts <robotros1@msn.com>
2020-01-28 15:17:06 -0800

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,10 +29,11 @@ class Home extends React.Component {
             </div>
           </div>
           <div className="row text-center">
-            <div className="col-md-6">
+            <div className='col-md-2'></div>
+            <div className="col-md-4 col-md-offset-2">
                       <p><span className="text-red">R</span>efrigeration <span className="text-black">C</span>ontracting <span className="text-blue">C</span>ompany was started by Bill Valenzuela after leaving California and moving to Arizona. Bill Valenzuela has over 40 years of experience in the commercial refrigeration industry and a reputation of outstanding Customer Service.</p>
                   </div>
-                  <div className="col-md-6">
+                  <div className="col-md-4">
                       <p><span className="text-red">R</span>efrigeration <span className="text-black">C</span>ontracting <span className="text-blue">C</span>ompany looks to continue the reputation of Bill Valenzuela and become a leading contractor in the Phoenix AZ area with the best customer service experience.</p>
                   </div>
                   <div className="col-md-2">
